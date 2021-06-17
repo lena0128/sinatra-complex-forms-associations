a7c8ce9804911e439b2be412a83f11f7757062af
Lena Hou <lenahou2013@gmail.com>
2021-06-17 12:16:39 -0400

    commit tree cf586e23c1d57b0c548903bee717c01387c8de2f

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,20 +1,26 @@
 class OwnersController < ApplicationController
-
+  
   get '/owners' do
     @owners = Owner.all
     erb :'/owners/index' 
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
+    @pets = Pet.all 
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
@@ -22,8 +28,18 @@ class OwnersController < ApplicationController
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
-
+ 
   patch '/owners/:id' do 
-   
+    # if !params[:owner].keys.include?("pet_ids")
+    #   params[:owner]["pet_ids"] = [ ]
+    # end
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
   end
+   
+ 
 end
